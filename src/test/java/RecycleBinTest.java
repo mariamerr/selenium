f4ca8f85acf06a1ad5c4766ae842ee3960fdcb83
Maria <mermarand@mail.ru>
2019-08-06 00:18:38 +0300

    refactor

--- src/test/java/RecycleBinTest.java
+++ src/test/java/RecycleBinTest.java
@@ -3,43 +3,42 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class RecycleBinTest extends TestBase {
 
-
-    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-    WebDriver driver = new ChromeDriver();
-
-//    private WebDriverWait wait;
-//    wait = new WebDriverWait(driver, ofSeconds(10));
-
+   private WebDriverWait wait;
 
     @Before
     public void login() {
         driver.get("http://litecart.stqa.ru/en/");
+        wait = new WebDriverWait(driver, ofSeconds(10));
     }
 
     @Test
     public void addToRecycle() {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i <= 3; i++) {
             driver.findElement(By.cssSelector("#box-most-popular ul li:nth-child(1)")).click();
-            String beforeAdd = driver.findElement(By.cssSelector("#cart > a.content")).getText();
+            if (isElementPresent(By.name("options[Size]"))) {
+                Select size = new Select(driver.findElement(By.name("options[Size]")));
+                size.selectByIndex(1);
+            }
             driver.findElement(By.name("add_cart_product")).click();
-            String afterAdd = driver.findElement(By.cssSelector("#cart > a.content")).getText();
-            Assert.assertNotEquals(beforeAdd, afterAdd);
+            wait.until(textToBe(By.cssSelector("#cart .quantity"), String.valueOf(i)));
             driver.get("http://litecart.stqa.ru/en/");
         }
         driver.findElement(By.cssSelector("#cart > a.link")).click();
-            for (int i = 0; i < 3; i++) {
-                String totalPrice = driver.findElement(By.cssSelector("#cart > a.content")).getText();
-                driver.findElement(By.name("remove_cart_item")).click();
-                String priceAfterRemove = driver.findElement(By.cssSelector(".footer > td:nth-child(2)")).getText();
-                Assert.assertNotEquals(totalPrice, priceAfterRemove);
-            }
+        while (isElementPresent(By.cssSelector(".dataTable"))) {
+            WebElement table = driver.findElement(By.cssSelector(".dataTable"));
+            driver.findElement(By.name("remove_cart_item")).click();
+            wait.until(stalenessOf(table));
+        }
     }
 }
